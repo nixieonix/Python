159b51aa4ba0ac94e0b56b93e5d8c6e3d5224b49
Nika Grabnar <nika.grabnar@gmail.com>
2015-10-06 16:18:30 +0200

    Dva igralca tekmujeta v metanju kocke. Kod bo hitreje prišel iz 20 točk do 0.

--- diceRoller.py
+++ diceRoller.py
@@ -8,15 +8,49 @@ player2 = []
 dice2 = randint(1,6)
 player2.append(dice2)
 
-print "Player 1: " + str(player1[0])
-print "Player 2: " + str(player2[0])
 
-if player1 < player2:
+
+ply1score = 20
+ply2score = 20
+
+while ply1score > 0 or ply2score > 0:
+	if ply1score != 0 and ply2score != 0:
+		dice1 = randint(1,6)
+		dice2 = randint(1,6)
+		
+		if ply1score < dice1 or ply2score < dice2:
+			print "Player 1: " + str(dice1)
+			print "Player 2: " + str(dice2)
+		else:
+			ply1score = ply1score - dice1
+			ply2score = ply2score - dice2
+
+		print "Player 1: " + str(dice1) + " Score: " + str(ply1score)
+		print "Player 2: " + str(dice2) + " Score: " + str(ply2score)
+
+
+	else: 
+		if ply1score == 0 and ply2score != 0:
+			print "Igra je koncana, zmagal je igralec 1!"
+			break
+		elif ply2score == 0 and ply1score != 0:
+			print "Igra je koncana, zmagal je igralec 2!"
+			break
+		else:
+			print "Igra je nedolocena!"
+			break
+
+
+
+
+
+"""if player1 < player2:
 	print "Player 2 wins!"
 elif player1 > player2:
 	print "Player 1 wins!"
 elif player1 == player2:
 	print "Match Draw!"
+"""
 
 
 """question = raw_input("Are you ready? ")
